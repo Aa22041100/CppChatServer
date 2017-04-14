a49878e528b2324ded104083b236c1765a237555
Stanley Lai <stanley.lai.104@gmail.com>
2017-04-14 17:17:20 +0800

    hello world

---
+++ CppChatServer/CppChatServer/CppChatServer.cpp
@@ -0,0 +1,13 @@
+// CppChatServer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	system("pause");
+    return 0;
+}
+
